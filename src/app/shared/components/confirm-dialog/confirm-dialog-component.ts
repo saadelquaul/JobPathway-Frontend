783867ac1228c58d7e972d760bac73e6a7ff97a1
SaadElQuaul <saadelquaul@gmail.com>
2026-03-18 14:49:59 +0000

    Add Confirm Dialog component with HTML, CSS, and TypeScript implementation

---
+++ src/app/shared/components/confirm-dialog/confirm-dialog-component.ts
@@ -0,0 +1,25 @@
+import { Component, input, output } from '@angular/core';
+
+@Component({
+  selector: 'app-confirm-dialog-component',
+  imports: [],
+  templateUrl: './confirm-dialog-component.html',
+  styleUrl: './confirm-dialog-component.css',
+})
+export class ConfirmDialogComponent {
+  open = input(false);
+  title = input('Confirm');
+  message = input('Are you sure?');
+  confirmText = input('Delete');
+
+  confirmed = output<void>();
+  cancelled = output<void>();
+
+  onConfirm(): void {
+    this.confirmed.emit();
+  }
+
+  onCancel(): void {
+    this.cancelled.emit();
+  }
+}
