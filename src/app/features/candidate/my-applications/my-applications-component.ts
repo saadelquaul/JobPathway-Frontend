96d65cd0dc63539439db620944e54ddb647796c1
SaadElQuaul <saadelquaul@gmail.com>
2026-03-22 15:19:26 +0100

    Add pagination support to My Applications component

--- src/app/features/candidate/my-applications/my-applications-component.ts
+++ src/app/features/candidate/my-applications/my-applications-component.ts
@@ -1,14 +1,16 @@
 import { Component, inject, OnInit, signal } from '@angular/core';
 import { LoadingSpinnerComponent } from '../../../shared/components/loading-spinner/loading-spinner-component';
 import { StatusBadgeComponent } from '../../../shared/components/status-badge/status-badge-component';
+import { PaginationComponent } from '../../../shared/components/pagination/pagination-component';
 import { DatePipe } from '@angular/common';
 import { ApplicationService } from '../../../core/services/application.service';
 import { Router } from '@angular/router';
 import { ApplicationResponse } from '../../../models/application.models';
+import { PageResponse } from '../../../models/pagination.models';
 
 @Component({
   selector: 'app-my-applications-component',
-  imports: [LoadingSpinnerComponent, StatusBadgeComponent, DatePipe],
+  imports: [LoadingSpinnerComponent, StatusBadgeComponent, PaginationComponent, DatePipe],
   templateUrl: './my-applications-component.html',
   styleUrl: './my-applications-component.css',
 })
@@ -19,16 +21,41 @@ export class MyApplicationsComponent implements OnInit {
   loading = signal(true);
   applications = signal<ApplicationResponse[]>([]);
 
+  // Pagination
+  currentPage = signal(0);
+  pageSize = signal(10);
+  totalPages = signal(0);
+  totalElements = signal(0);
+
   ngOnInit(): void {
-    this.appService.getMyApplications().subscribe({
-      next: (data) => {
-        this.applications.set(data);
+    this.loadApplications();
+  }
+
+  loadApplications(): void {
+    this.loading.set(true);
+    this.appService.getMyApplicationsPaginated(this.currentPage(), this.pageSize()).subscribe({
+      next: (page: PageResponse<ApplicationResponse>) => {
+        this.applications.set(page.content);
+        this.totalPages.set(page.totalPages);
+        this.totalElements.set(page.totalElements);
+        this.currentPage.set(page.number);
         this.loading.set(false);
       },
       error: () => {
         this.loading.set(false);
       },
     });
+  }
+
+  onPageChange(page: number): void {
+    this.currentPage.set(page);
+    this.loadApplications();
+  }
+
+  onPageSizeChange(size: number): void {
+    this.pageSize.set(size);
+    this.currentPage.set(0);
+    this.loadApplications();
   }
 }
 
