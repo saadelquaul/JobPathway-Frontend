c972fa8182f4f5d4fb22bbe5310395fdd41edcb1
SaadElQuaul <saadelquaul@gmail.com>
2026-03-22 15:19:58 +0100

    Add pagination models for PageResponse and PageRequest interfaces

---
+++ src/app/models/pagination.models.ts
@@ -0,0 +1,15 @@
+export interface PageResponse<T> {
+  content: T[];
+  totalElements: number;
+  totalPages: number;
+  size: number;
+  number: number;
+  first: boolean;
+  last: boolean;
+}
+
+export interface PageRequest {
+  page: number;
+  size: number;
+  sort?: string[];
+}
