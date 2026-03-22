16518ec0806c0a8afc9d211b965f0f6f8a902a75
SaadElQuaul <saadelquaul@gmail.com>
2026-03-22 15:17:53 +0100

    Add pagination support for applications retrieval in ApplicationService

--- src/app/core/services/application.service.ts
+++ src/app/core/services/application.service.ts
@@ -1,8 +1,9 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { environment } from '../../../environments/environment';
 import { ApplicationResponse, ApplicationStatusUpdateRequest } from '../../models/application.models';
+import { PageResponse } from '../../models/pagination.models';
 
 @Injectable({ providedIn: 'root' })
 export class ApplicationService {
@@ -18,8 +19,32 @@ export class ApplicationService {
     return this.http.get<ApplicationResponse[]>(`${this.apiUrl}/my-applications`);
   }
 
+  getMyApplicationsPaginated(page: number = 0, size: number = 10, sort: string[] = ['appliedAt,desc']): Observable<PageResponse<ApplicationResponse>> {
+    let params = new HttpParams()
+      .set('page', page.toString())
+      .set('size', size.toString());
+
+    sort.forEach(s => {
+      params = params.append('sort', s);
+    });
+
+    return this.http.get<PageResponse<ApplicationResponse>>(`${this.apiUrl}/my-applications`, { params });
+  }
+
   getApplicationsByJobOffer(jobOfferId: number): Observable<ApplicationResponse[]> {
     return this.http.get<ApplicationResponse[]>(`${this.apiUrl}/job-offer/${jobOfferId}`);
+  }
+
+  getApplicationsByJobOfferPaginated(jobOfferId: number, page: number = 0, size: number = 10, sort: string[] = ['appliedAt,desc']): Observable<PageResponse<ApplicationResponse>> {
+    let params = new HttpParams()
+      .set('page', page.toString())
+      .set('size', size.toString());
+
+    sort.forEach(s => {
+      params = params.append('sort', s);
+    });
+
+    return this.http.get<PageResponse<ApplicationResponse>>(`${this.apiUrl}/job-offer/${jobOfferId}`, { params });
   }
 
   updateApplicationStatus(id: number, req: ApplicationStatusUpdateRequest): Observable<ApplicationResponse> {
