9348b9f433a2b0ae0bd80858a00ed0502cce86cf
SaadElQuaul <saadelquaul@gmail.com>
2026-03-26 11:06:23 +0100

    Refactor admin layout to use a sticky navbar and enhance structure with notification bell integration

--- src/app/features/admin/layout/admin-layout-component.ts
+++ src/app/features/admin/layout/admin-layout-component.ts
@@ -1,10 +1,11 @@
 import { Component, inject } from '@angular/core';
 import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 import { AuthService } from '../../../core/services/auth.service';
+import { NotificationBellComponent } from '../../../shared/components/notification-bell/notification-bell-component';
 
 @Component({
   selector: 'app-admin-layout-component',
-  imports: [RouterOutlet, RouterLink, RouterLinkActive],
+  imports: [RouterOutlet, RouterLink, RouterLinkActive, NotificationBellComponent],
   templateUrl: './admin-layout-component.html',
   styleUrl: './admin-layout-component.css',
 })
