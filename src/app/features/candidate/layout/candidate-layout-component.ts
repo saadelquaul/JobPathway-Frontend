4261b2e0da2e23c4069619d8fe8a89f4736ba45a
SaadElQuaul <saadelquaul@gmail.com>
2026-03-26 11:07:11 +0100

    Refactor candidate layout to use a sticky navbar; integrate notification bell component and update styles

--- src/app/features/candidate/layout/candidate-layout-component.ts
+++ src/app/features/candidate/layout/candidate-layout-component.ts
@@ -1,10 +1,11 @@
 import { Component, inject } from '@angular/core';
 import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 import { AuthService } from '../../../core/services/auth.service';
+import { NotificationBellComponent } from '../../../shared/components/notification-bell/notification-bell-component';
 
 @Component({
   selector: 'app-candidate-layout-component',
-  imports: [RouterOutlet, RouterLink, RouterLinkActive],
+  imports: [RouterOutlet, RouterLink, RouterLinkActive, NotificationBellComponent],
   templateUrl: './candidate-layout-component.html',
   styleUrl: './candidate-layout-component.css',
 })
